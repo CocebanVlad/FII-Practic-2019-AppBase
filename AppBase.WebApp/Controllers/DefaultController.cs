7dd49f939f6eb6a7ec93ebe09de27124ac4e963d
vcoceban <vcoceban@BEE-WS-179>
2019-03-31 14:33:02 +0300

    add a new WebApp proj for our JS framework

---
+++ AppBase.WebApp/Controllers/DefaultController.cs
@@ -0,0 +1,13 @@
+﻿using System.Web.Mvc;
+
+namespace AppBase.WebApp.Controllers
+{
+    public class DefaultController : Controller
+    {
+        [HttpGet]
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
