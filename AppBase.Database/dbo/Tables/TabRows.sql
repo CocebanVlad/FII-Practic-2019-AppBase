e63cb9ab337eb58bfba0ef3dc9766a7b96702af8
vcoceban <vcoceban@BEE-WS-179>
2019-03-31 15:26:49 +0300

    update DB proj

--- AppBase.Database/dbo/Tables/TabRows.sql
+++ AppBase.Database/dbo/Tables/TabRows.sql
@@ -3,6 +3,7 @@
     [Cod]         NVARCHAR (120) NOT NULL,
     [Description] NVARCHAR (120) NOT NULL,
     CONSTRAINT [PK_TabRows] PRIMARY KEY CLUSTERED ([CodTab] ASC, [Cod] ASC),
-    CONSTRAINT [FK_TabRows_Tab] FOREIGN KEY ([CodTab]) REFERENCES [dbo].[Tabs] ([Cod])
+    CONSTRAINT [FK_TabRows_Tab] FOREIGN KEY ([CodTab]) REFERENCES [dbo].[Tabs] ([Cod]),
+    CONSTRAINT [FK_TabRows_TabRowDetails] FOREIGN KEY ([CodTab], [Cod]) REFERENCES [dbo].[TabRowDetails] ([CodTab], [Cod])
 );
 
